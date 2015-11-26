926b54c93b7f18f378a38a471012eee7cd11e729
Thomas 🐢 Wessel <Thomas@thomass-imac.local>
2015-11-26 14:45:43 -0800

    Initial Commit

---
+++ The Turtle Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The Turtle Game
+//
+//  Created by Thomas Wessel on 11/26/15.
+//  Copyright © 2015 Glint Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ The Turtle Game/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  The Turtle Game
+//
+
+//  Copyright (c) 2015 Glint Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
